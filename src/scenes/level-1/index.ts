7c05c1bbe9fd5de2567e1ce4c3beedbdb37a2c21
Niall Eccles <nialle@rothco.ie>
2021-08-08 23:09:59 +0100

    Added camera to Level 1. Camera follows player.

--- src/scenes/level-1/index.ts
+++ src/scenes/level-1/index.ts
@@ -10,8 +10,15 @@ export class Level1 extends Phaser.Scene {
     
   }
 
+  private initCamera(): void {
+    this.cameras.main.setSize(this.game.scale.width, this.game.scale.height);
+    this.cameras.main.startFollow(this.player, true, 0.09, 0.09);
+    this.cameras.main.setZoom(2);
+  }
+
   create(): void {
     this.player = new Player(this, 100,100);
+    this.initCamera();
   }
 
   update(): void{
